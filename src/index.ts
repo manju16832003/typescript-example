43a680a99ab36cfddce24fa71fa7f34aa3bee017
Manjunath Reddy <manju16832003@gmail.com>
2018-02-16 17:58:40 +0800

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello Typescript')
+import express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello TypeScript')
+})
+
+console.log('Listening to port 3004')
+app.listen(3004)
